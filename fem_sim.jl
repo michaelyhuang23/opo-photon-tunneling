76b478dd89f351a9ae849fc03c955a443416e3cf
Michael Huang <michaelyhuang23@gmail.com>
2024-10-13 15:05:01 -0400

    creation

---
+++ fem_sim.jl
@@ -0,0 +1,180 @@
+using Ferrite, SparseArrays, LinearAlgebra, PolynomialRoots, Arpack, Plots, StaticArrays, Statistics
+
+
+# constants
+b_val = 0.5
+lambda_val = 1.8
+g_val = 0.1
+div_val = 0
+
+function parametrize(alpha, beta)
+    u = asin(g_val * alpha / sqrt(lambda_val)) + asin(g_val * beta / sqrt(lambda_val))
+    v = asin(g_val * alpha / sqrt(lambda_val)) - asin(g_val * beta / sqrt(lambda_val))
+    return [u, v]
+end
+
+alpha_roots = roots([-b_val, -(lambda_val - 1), 0, g_val^2])
+alpha_roots = sort(map(x -> real(x), alpha_roots))
+u_roots = map(x -> parametrize(x, x)[1], alpha_roots)
+
+function V(u, v)
+    term1 = lambda_val * (cos(u) - cos(v)) - 2 * log(cos(u) + cos(v))
+    term2 = 2 * g_val * b_val / sqrt(lambda_val) * log(cos(u) + cos(v))
+    term3 = -2 * g_val * b_val / sqrt(lambda_val) * log(2 + 2 * sin((u + v) / 2) + 2 * sin((u - v) / 2) + cos(v) - cos(u))
+    return term1 + term2 + term3
+end
+
+function Vuu(alpha)
+    return (lambda_val - g_val^2 * b_val * alpha) / (lambda_val - g_val^2 * alpha^2) - lambda_val + 2 * g_val^2 * alpha^2
+end
+
+function Vvv(alpha)
+    return (lambda_val - g_val^2 * b_val * alpha) / (lambda_val - g_val^2 * alpha^2) + lambda_val
+end
+
+function V_grad(u, v)
+    du = tan((u + v) / 2) + tan((u - v) / 2) - g_val * b_val / sqrt(lambda_val) * (sec((u + v) / 2) + sec((u - v) / 2)) - lambda_val * sin(u)
+    dv = tan((u + v) / 2) - tan((u - v) / 2) - g_val * b_val / sqrt(lambda_val) * (sec((u + v) / 2) - sec((u - v) / 2)) + lambda_val * sin(v)
+    return [du, dv]
+end
+
+function beta_A_calc()
+    Va = V(u_roots[1], 0)
+    Vb = V(u_roots[3], 0)
+    term1 = exp((Va - Vb) / g_val^2)
+    term2 = sqrt(Vuu(alpha_roots[1]) * Vvv(alpha_roots[1]) / Vuu(alpha_roots[3]) / Vvv(alpha_roots[3]))
+    return term1 * term2
+end
+
+
+beta_B = -1.0
+beta_A = beta_A_calc()
+
+function div_J(u, v)
+    radius = 0.005
+    if (u - u_roots[1])^2 + v^2 < radius^2
+        return div_val
+    elseif (u - u_roots[3])^2 + v^2 < radius^2
+        return -div_val
+    else
+        return 0
+    end
+end
+
+println("beta_A: ", beta_A)
+println("beta_B: ", beta_B)
+
+# create grid and basis functions
+grid = generate_grid(Quadrilateral, (400, 400), Vec(-2.0, -1.0), Vec(2.0, -1.0), Vec(2.0, 1.0), Vec(-2.0, 1.0))
+dim = 2
+polynomials = Lagrange{dim,RefCube,1}()
+quadrature_rules = QuadratureRule{dim,RefCube}(2)
+quadrature_rules_face = QuadratureRule{dim - 1,RefCube}(2)
+cell_values = CellScalarValues(quadrature_rules, polynomials)
+facevalues = FaceScalarValues(quadrature_rules_face, polynomials);
+
+# add beta field
+dof_handler = DofHandler(grid)
+add!(dof_handler, :beta, 1)
+close!(dof_handler)
+
+# add dirichlet boundary condition
+constraint_handler = ConstraintHandler(dof_handler)
+# addvertexset!(grid, "LL", x -> x[1] ≈ -2.0 && x[2] ≈ -1.0)
+l_boundary_condition = Dirichlet(:beta, getfaceset(grid, "left"), (x, t) -> beta_A)
+r_boundary_condition = Dirichlet(:beta, getfaceset(grid, "right"), (x, t) -> beta_B)
+add!(constraint_handler, l_boundary_condition)
+add!(constraint_handler, r_boundary_condition)
+close!(constraint_handler)
+
+
+function assemble_element!(Ke::Matrix, Fe::Vector, cellvalues::CellScalarValues, cell::CellCache, cellcount::Int, facevalues::FaceScalarValues)
+    n_basefuncs = getnbasefunctions(cellvalues)
+    # Reset to 0
+    fill!(Ke, 0)
+    fill!(Fe, 0)
+    cell_coords = getcoordinates(cell)
+    # Loop over quadrature points
+    for q_point in 1:getnquadpoints(cellvalues)
+        # Get the quadrature weight
+        dOmega = getdetJdV(cellvalues, q_point)
+        point_coord = spatial_coordinate(cellvalues, q_point, cell_coords)
+        # Loop over test basis
+        for i in 1:n_basefuncs
+            g_phi = shape_value(cellvalues, q_point, i)
+            g_phi_grad = shape_gradient(cellvalues, q_point, i)
+            # Add contribution to Fe
+            Fe[i] += g_phi * div_J(point_coord[1], point_coord[2]) * exp(V(point_coord[1], point_coord[2]) / g_val^2) / g_val^2 * dOmega
+            # Loop over trial basis
+            for j in 1:n_basefuncs
+                beta_phi_grad = shape_gradient(cellvalues, q_point, j)
+                # Add contribution to Ke
+                Ke[i, j] += g_phi * dot(beta_phi_grad, V_grad(point_coord[1], point_coord[2])) / g_val^2 * dOmega
+                Ke[i, j] += dot(g_phi_grad, beta_phi_grad) * dOmega
+            end
+        end
+    end
+    # add von neumann condition
+    for face in 1:nfaces(cell)
+        if onboundary(cell, face) #&&
+            # ((cellcount, face) in getfaceset(grid, "bottom") ||
+            #  (cellcount, face) in getfaceset(grid, "top"))
+            reinit!(facevalues, cell, face)
+            for q_point in 1:getnquadpoints(facevalues)
+                dGamma = getdetJdV(facevalues, q_point)
+                point_coord = spatial_coordinate(facevalues, q_point, cell_coords)
+                n = getnormal(facevalues, q_point)
+                # Loop over test basis
+                for i in 1:n_basefuncs
+                    g_phi = shape_value(facevalues, q_point, i)
+                    # Loop over trial basis
+                    for j in 1:n_basefuncs
+                        beta_phi_grad = shape_gradient(facevalues, q_point, j)
+                        Ke[i, j] -= g_phi * dot(beta_phi_grad, n) * dGamma
+                        # comment out to impose von neummann condition
+                    end
+                end
+            end
+        end
+    end
+    return Ke
+end
+
+function assemble_global(cellvalues::CellScalarValues, facevalues::FaceScalarValues, K::SparseMatrixCSC, dh::DofHandler)
+    # Allocate the element stiffness matrix and element force vector
+    n_basefuncs = getnbasefunctions(cellvalues)
+    Ke = zeros(n_basefuncs, n_basefuncs)
+    Fe = zeros(n_basefuncs)
+
+    f = zeros(ndofs(dof_handler))
+    # Create an assembler
+    assembler = start_assemble(K, f)
+    # Loop over all cels
+    for (cellcount, cell) in enumerate(CellIterator(dh))
+        # Reinitialize cellvalues for this cell
+        reinit!(cellvalues, cell)
+        # Compute element contribution
+        assemble_element!(Ke, Fe, cellvalues, cell, cellcount, facevalues)
+        # Assemble Ke and fe into K and f
+        assemble!(assembler, celldofs(cell), Ke, Fe)
+    end
+    return K, f
+end
+
+K = create_sparsity_pattern(dof_handler)  # create structure
+K, f = assemble_global(cell_values, facevalues, K, dof_handler)
+
+apply!(K, f, constraint_handler)
+
+beta_sol = K \ f
+
+# beta_sol = (beta_sol .- mean(beta_sol)) ./ std(beta_sol)
+
+#beta_sol = clamp.(beta_sol, 0.8, 1)
+
+vtk_grid("fem_sim", dof_handler) do vtk
+    vtk_point_data(vtk, dof_handler, beta_sol)
+end
+
+
+
---
+++ sol_compare.jl
@@ -0,0 +1,159 @@
+using DifferentialEquations
+using Plots
+using PolynomialRoots
+using JSON
+using LsqFit
+
+
+function calc_time(b_val, lambda_val, g_val)
+    function parametrize(alpha, beta)
+        u = asin(g_val * alpha / sqrt(lambda_val)) + asin(g_val * beta / sqrt(lambda_val))
+        v = asin(g_val * alpha / sqrt(lambda_val)) - asin(g_val * beta / sqrt(lambda_val))
+        return [u, v]
+    end
+
+    function sol1_time()
+        function V(u, v)
+            term1 = lambda_val * (cos(u) - cos(v)) - 2 * log(cos(u) + cos(v))
+            term2 = 2 * g_val * b_val / sqrt(lambda_val) * log(cos(u) + cos(v))
+            term3 = -2 * g_val * b_val / sqrt(lambda_val) * log(2 + 2 * sin((u + v) / 2) + 2 * sin((u - v) / 2) + cos(v) - cos(u))
+            return term1 + term2 + term3
+        end
+        function Vuu(alpha)
+            return (lambda_val - g_val^2 * b_val * alpha) / (lambda_val - g_val^2 * alpha^2) - lambda_val + 2 * g_val^2 * alpha^2
+        end
+        function Vvv(alpha)
+            return (lambda_val - g_val^2 * b_val * alpha) / (lambda_val - g_val^2 * alpha^2) + lambda_val
+        end
+        alpha_roots = roots([-b_val, -(lambda_val - 1), 0, g_val^2])
+        alpha_roots = sort(map(x -> real(x), alpha_roots))
+        u_roots = map(x -> parametrize(x, x)[1], alpha_roots)
+        Vs = V(u_roots[2], 0)
+        Va = V(u_roots[1], 0)
+        Vb = V(u_roots[3], 0)
+
+        term1 = 2 * pi * exp((Vs - Va) / g_val^2) / sqrt(Vuu(alpha_roots[1]) * Vvv(alpha_roots[1]))
+        term2 = 2 * pi * exp((Vs - Vb) / g_val^2) / sqrt(Vuu(alpha_roots[3]) * Vvv(alpha_roots[3]))
+        term3 = sqrt(Vvv(alpha_roots[2]) / (-Vuu(alpha_roots[2])))
+        term4 = exp((Va - Vb) / g_val^2)
+        term5 = sqrt(Vuu(alpha_roots[1]) * Vvv(alpha_roots[1]) / Vuu(alpha_roots[3]) / Vvv(alpha_roots[3]))
+
+        return (term1 + term2) * term3 / (term4 * term5 + 1)
+    end
+    return sol1_time()
+end
+
+function find_b_vals(lambda_val, g_val)
+    left = BigFloat(0.0)
+    right = BigFloat(4.0*10^10)
+    possible_b_vals = []
+    while right - left > BigFloat(1e-9)
+        mid = (left + right) / 2
+        try
+            time = calc_time(Float64(mid), lambda_val, g_val)
+            if time < 400
+                right = mid
+                push!(possible_b_vals, (Float64(mid), time))
+            else
+                left = mid
+            end
+        catch e
+            right = mid
+        end
+        if length(possible_b_vals) > 2
+            break
+        end
+    end
+    return possible_b_vals
+end
+
+cases = []
+lambda_val = 2.0
+b_val = 0.0
+for g_val in range(0.4, 1.0, 20)
+    try
+        time = calc_time(b_val, lambda_val, g_val)
+        if time < 400
+            push!(cases, Dict("g" => g_val, "lambda" => lambda_val, "b" => b_val, "analytic_time" => time))
+            println("possible params: g: $g_val, lambda: $lambda_val, b: $b_val, time: $time")
+        end
+    catch e
+        right = mid
+    end
+end
+
+
+function sim_time(b_val, lambda_val, g_val, t_sim_end)
+    function drift!(dalpha, alpha, p, t)
+        dalpha[1] = b_val - alpha[1] + alpha[2] * (lambda_val - g_val^2 * alpha[1]^2)
+        dalpha[2] = b_val - alpha[2] + alpha[1] * (lambda_val - g_val^2 * alpha[2]^2)
+    end
+
+    function noise!(dalpha, alpha, p, t)
+        dalpha[1, 1] = sqrt(lambda_val - g_val^2 * alpha[1]^2)
+        dalpha[2, 2] = sqrt(lambda_val - g_val^2 * alpha[2]^2)
+        dalpha[1, 2] = 0.0
+        dalpha[2, 1] = 0.0
+    end
+
+    t_span = (0.0, t_sim_end)
+    alpha_0 = [Complex(-sqrt(lambda_val - 1) / g_val, 0.0), Complex(-sqrt(lambda_val - 1) / g_val, 0.0)]
+    alpha_f = [Complex(sqrt(lambda_val - 1) / g_val, 0.0), Complex(sqrt(lambda_val - 1) / g_val, 0.0)]
+    W = RealWienerProcess!(0.0, zeros(2), zeros(2))
+    problem = SDEProblem(drift!, noise!, alpha_0, t_span, noise_rate_prototype=complex(zeros(2, 2)), noise=W)
+    sol = solve(problem)
+    ensamble_prob = EnsembleProblem(problem)
+
+    function condition(alpha, t, integrator)
+        return real(alpha[1] + conj(alpha[2]))/2 > 0.0
+    end
+
+    function affect!(integrator)
+        terminate!(integrator)
+    end
+
+    cb = ContinuousCallback(condition, affect!)
+    sol = solve(ensamble_prob, EM(), dt=0.003 ,trajectories=100, callback=cb, EnsembleThreads(), adaptive=false)
+    successful_trajs = [traj for traj in sol if Symbol(traj.retcode) == :Success || Symbol(traj.retcode) == :Terminated]
+    min_len = minimum([length(traj.t) for traj in successful_trajs])
+    mean_amps = []
+    mean_ts = []
+    for traj in successful_trajs
+        amps = map(x -> -real(x[1]+conj(x[2])), traj.u)
+        if length(mean_amps) == 0
+            mean_amps = amps[1:min_len]
+            mean_ts = traj.t[1:min_len]
+        else
+            mean_amps = mean_amps .+ amps[1:min_len]
+            mean_ts = mean_ts .+ traj.t[1:min_len]
+        end
+    end
+    mean_amps ./= length(successful_trajs)
+    mean_ts ./= length(successful_trajs)
+    cut_idx = findfirst(x -> x<0.0, mean_amps)
+    decay_model(x, p) = p[1] .* exp.(-p[2] .* x)
+    fit = curve_fit(decay_model, mean_ts[1:cut_idx], mean_amps[1:cut_idx], [1.0, 1.0])
+    fit_time = 1 / coef(fit)[2]
+    end_time = 1.0#sum(end_times) / length(end_times)
+    println("g: $g_val, lambda: $lambda_val, b: $b_val")
+    println("success count: $(length(successful_trajs)), end times: $end_time, fit time: $fit_time")
+    return end_time, fit_time
+end
+
+
+success_cases = []
+for case in cases
+    try
+        end_time, fit_time = sim_time(case["b"], case["lambda"], case["g"], case["analytic_time"] * 4)
+        case["sim_end_time"] = end_time
+        case["sim_fit_time"] = fit_time
+        push!(success_cases, case)
+    catch e
+        println(e)
+    end
+end
+
+println("number of successes: ", size(success_cases))
+open("output.json", "w") do file
+    JSON.print(file, success_cases)
+end
